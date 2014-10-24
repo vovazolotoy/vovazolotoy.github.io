ec6449f49e5d9dd32231a10962f19fa66d82fb60
Vova Zolotoy <vovazolotoy@gmail.com>
2014-10-24 10:01:25 -0700

    still docs update

--- docs/api.js
+++ docs/api.js
@@ -2,6 +2,11 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "DoublyLinkedList",
+        "Heap",
+        "MaxHeap",
+        "MinHeap",
+        "PriorityQueue",
+        "Queue",
         "Stack"
     ],
     "modules": [],
